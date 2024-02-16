c258b7cbba4a04cb21d4264dac52e517c2d1c193
Renato <reytuty@gmail.com>
2024-02-16 17:41:16 -0300

    chore: upgrade token calc value

--- src/examples/calcTokenValue.example.ts
+++ src/examples/calcTokenValue.example.ts
@@ -2,7 +2,7 @@ import { TokenInfoInterface, calcTokenValue } from "..";
 
 const token: TokenInfoInterface = {
   minValue: 100,
-  availableTokens: 1000000,
+  availableTokens: 100000,
   slotUnitValue: 100,
   valuePerToken: 1,
 };
